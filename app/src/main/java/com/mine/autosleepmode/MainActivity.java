344c92d60bea924d45537e3b069e1f69dde2c474
The-First-King <59452275+The-First-King@users.noreply.github.com>
2025-12-27 13:18:42 -0500

    Update MainActivity.java

--- app/src/main/java/com/mine/autosleepmode/MainActivity.java
+++ app/src/main/java/com/mine/autosleepmode/MainActivity.java
@@ -22,7 +22,8 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.util.Locale;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity
+{
     private static final String TAG = "MainActivity";
 
     private final AlarmBroadcastReceiver sleepBroadcastReceiver = new AlarmBroadcastReceiver();
@@ -32,7 +33,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Root logic with automatic permission grant
         if (RootUtil.isDeviceRooted()) {
             try {
                 Process p = Runtime.getRuntime().exec("su");
@@ -41,7 +41,7 @@ public class MainActivity extends Activity {
                 os.writeBytes("exit\n");
                 os.flush();
             } catch (RuntimeException | IOException e) {
-                Log.e(TAG, "Exception during root permission grant: " + e.getMessage());
+                Log.e(TAG, "Exception :( " + e.getMessage());
             }
         } else {
             new AlertDialog.Builder(MainActivity.this)
@@ -57,10 +57,8 @@ public class MainActivity extends Activity {
                     }
                 }).show();
         }
-
         SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
 
-        // Setup Enable Sleep Switch
         Switch switchEnableSleep = findViewById(R.id.switchEnableSleep);
         switchEnableSleep.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
@@ -72,18 +70,16 @@ public class MainActivity extends Activity {
                 if (isChecked) {
                     sleepBroadcastReceiver.setAlarmEnableSleepMode(MainActivity.this);
                 } else {
-                    displayToast("Auto Sleep Mode activation disabled");
+                    displayToast("This device won't turn on Sleep Mode any more");
                 }
 
                 SharedPreferences s = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
                 SharedPreferences.Editor editor = s.edit();
                 editor.putBoolean(Constants.AUTOMATIC_ENABLE, isChecked);
                 editor.apply();
-                updateNextDay();
             }
         });
 
-        // Setup Disable Sleep Switch
         Switch switchDisableSleep = findViewById(R.id.switchDisableSleep);
         switchDisableSleep.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
              @Override
@@ -95,90 +91,81 @@ public class MainActivity extends Activity {
                  if (isChecked) {
                      sleepBroadcastReceiver.setAlarmDisableSleepMode(MainActivity.this);
                  } else {
-                     displayToast("Auto Sleep Mode deactivation disabled");
+                     displayToast("This device won't turn off Sleep Mode any more");
                  }
 
                  SharedPreferences s = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
                  SharedPreferences.Editor editor = s.edit();
                  editor.putBoolean(Constants.AUTOMATIC_DISABLE, isChecked);
                  editor.apply();
-                 updateNextDay();
              }
         });
 
-        // Time picker for enabling sleep
         final TextView editEnableSleep = findViewById(R.id.editEnableSleep);
         editEnableSleep.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String current = editEnableSleep.getText().toString();
-                int h = Integer.parseInt(current.split(":")[0]);
-                int m = Integer.parseInt(current.split(":")[1]);
 
                 new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
                     @Override
                     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
                         editEnableSleep.setText(String.format(Locale.getDefault(), "%02d:%02d", hourOfDay, minute));
                         saveClocks();
-                        updateNextDay();
                         sleepBroadcastReceiver.cancelAlarm(MainActivity.this, Constants.ID_ENABLE);
                         sleepBroadcastReceiver.setAlarmEnableSleepMode(MainActivity.this);
                     }
-                }, h, m, true).show();
+                }, 23, 0, true).show();
             }
         });
 
-        // Time picker for disabling sleep
         final TextView editDisableSleep = findViewById(R.id.editDisableSleep);
         editDisableSleep.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String current = editDisableSleep.getText().toString();
-                int h = Integer.parseInt(current.split(":")[0]);
-                int m = Integer.parseInt(current.split(":")[1]);
 
                 new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
                     @Override
                     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
                         editDisableSleep.setText(String.format(Locale.getDefault(), "%02d:%02d", hourOfDay, minute));
                         saveClocks();
-                        updateNextDay();
                         sleepBroadcastReceiver.cancelAlarm(MainActivity.this, Constants.ID_DISABLE);
                         sleepBroadcastReceiver.setAlarmDisableSleepMode(MainActivity.this);
                     }
-                }, h, m, true).show();
+                }, 8, 0, true).show();
             }
         });
 
-        // Initialize UI from settings
         editEnableSleep.setText(settings.getString(Constants.ENABLE_SLEEP_TIME, "23:00"));
         editDisableSleep.setText(settings.getString(Constants.DISABLE_SLEEP_TIME, "08:00"));
+
         switchEnableSleep.setChecked(settings.getBoolean(Constants.AUTOMATIC_ENABLE, false));
         switchDisableSleep.setChecked(settings.getBoolean(Constants.AUTOMATIC_DISABLE, false));
-        
-        editEnableSleep.setEnabled(switchEnableSleep.isChecked());
-        editDisableSleep.setEnabled(switchDisableSleep.isChecked());
-        updateNextDay();
     }
 
     private void displayToast(String message) {
         Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu (Menu menu) {
         getMenuInflater().inflate(R.menu.settings, menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Matches the IDs in your menu/settings.xml
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
-            startActivity(intent);
-            return true;
+        switch (item.getItemId()) {
+            case R.id.menu_about:
+                new AlertDialog.Builder(MainActivity.this)
+                        .setMessage(getString(R.string.about_description_main) + "\n\n" + getString(R.string.about_description_sleep_well))
+                        .setCancelable(true)
+                        .setPositiveButton(R.string.about_ok_button, null).show();
+                break;
+            case R.id.menu_settings:
+                Intent aboutIntent = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(aboutIntent);
+                break;
         }
         return super.onOptionsItemSelected(item);
     }
@@ -193,22 +180,18 @@ public class MainActivity extends Activity {
         final TextView editEnableSleep = findViewById(R.id.editEnableSleep);
         final TextView editDisableSleep = findViewById(R.id.editDisableSleep);
         final TextView nextDay = findViewById(R.id.nextDay);
-        
-        try {
-            String[] e = editEnableSleep.getText().toString().split(":");
-            String[] d = editDisableSleep.getText().toString().split(":");
-            int eHour = Integer.parseInt(e[0]);
-            int eMinute = Integer.parseInt(e[1]);
-            int dHour = Integer.parseInt(d[0]);
-            int dMinute = Integer.parseInt(d[1]);
-
-            if ((dHour < eHour) || (dHour == eHour && dMinute < eMinute)) {
-                nextDay.setVisibility(View.VISIBLE);
-            } else {
-                nextDay.setVisibility(View.INVISIBLE);
-            }
-        } catch (Exception ex) {
-            Log.e(TAG, "Error calculating next day visibility");
+        String enable = editEnableSleep.getText().toString();
+        String disable = editDisableSleep.getText().toString();
+        String[] e = enable.split(":");
+        String[] d = disable.split(":");
+        int eHour = Integer.valueOf(e[0]);
+        int eMinute = Integer.valueOf(e[1]);
+        int dHour = Integer.valueOf(d[0]);
+        int dMinute = Integer.valueOf(d[1]);
+        if ((dHour < eHour) || (dHour == eHour && dMinute < eMinute)) {
+            nextDay.setVisibility(View.VISIBLE);
+        } else {
+            nextDay.setVisibility(View.INVISIBLE);
         }
     }
 
@@ -219,6 +202,8 @@ public class MainActivity extends Activity {
         final TextView editDisableSleep = findViewById(R.id.editDisableSleep);
         editor.putString(Constants.ENABLE_SLEEP_TIME, editEnableSleep.getText().toString());
         editor.putString(Constants.DISABLE_SLEEP_TIME, editDisableSleep.getText().toString());
+        Log.d(TAG, "enable sleep mode at: " + editEnableSleep.getText().toString());
+        Log.d(TAG, "disable sleep mode at: " + editDisableSleep.getText().toString());
         editor.apply();
     }
 }
