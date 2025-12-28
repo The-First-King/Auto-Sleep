ad41f59cbf2864225d67328506105edc1c8de031
The-First-King <59452275+The-First-King@users.noreply.github.com>
2025-12-28 13:56:33 -0500

    Update Constants.java

--- app/src/main/java/com/mine/autosleep/Constants.java
+++ app/src/main/java/com/mine/autosleep/Constants.java
@@ -1,14 +1,13 @@
 package com.mine.autosleep;
 
-final class Constants
-{
+public class Constants {
     static final String APP_IS_ENABLED = "appEnabled";
     static final String ENABLE_SLEEP_TIME = "enableSleepTime";
     static final String DISABLE_SLEEP_TIME = "disableSleepTime";
+    static final String START_ON_NEXT_DAY = "startOnNextDay"; 
 
-    static final int ID_ENABLE = 1;
-    static final int ID_DISABLE = 2;
-
-    static final String ID = "id";
-    static final String END = "end";
+    public static final String ID = "id";
+    public static final int ID_ENABLE = 1;
+    public static final int ID_DISABLE = 2;
+    public static final String END = "end";
 }
