e17e17b202d3605e919ef35edc1a44f4c3785f8a
The-First-King <59452275+The-First-King@users.noreply.github.com>
2026-01-23 13:29:15 -0500

    Update Constants.java

--- app/src/main/java/com/mine/autosleep/Constants.java
+++ app/src/main/java/com/mine/autosleep/Constants.java
@@ -2,16 +2,18 @@ package com.mine.autosleep;
 
 public class Constants {
 
-    // Existing keys (keep)
     static final String APP_IS_ENABLED = "appEnabled";
     static final String ENABLE_SLEEP_TIME = "enableSleepTime";
     static final String DISABLE_SLEEP_TIME = "disableSleepTime";
     static final String START_ON_NEXT_DAY = "startOnNextDay";
-
+	static final String PREF_SCHEDULED_EXIT_AT = "scheduled_exit_at_epoch";
     public static final String ID = "id";
     public static final int ID_ENABLE = 1;
     public static final int ID_DISABLE = 2;
     public static final String END = "end";
+    public static final String ORIGIN = "origin";
+    public static final String ORIGIN_ALARM = "alarm";
+    public static final String ORIGIN_MANUAL = "manual";
 
     // Sleep mode state + snapshot (restore always)
     static final String PREF_SLEEP_ACTIVE = "sleepActive";
