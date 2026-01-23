5070ceb3825ef33e31e5ef4ded7c760d6e7e9ffb
The-First-King <59452275+The-First-King@users.noreply.github.com>
2026-01-23 13:30:29 -0500

    Update ExitNowReceiver.java

--- app/src/main/java/com/mine/autosleep/ExitNowReceiver.java
+++ app/src/main/java/com/mine/autosleep/ExitNowReceiver.java
@@ -6,9 +6,10 @@ import android.content.Intent;
 
 public class ExitNowReceiver extends BroadcastReceiver {
     @Override
-       public void onReceive(Context context, Intent intent) {
+    public void onReceive(Context context, Intent intent) {
         Intent work = new Intent(context, AutoSleepService.class);
         work.putExtra(Constants.ID, Constants.ID_DISABLE);
+        work.putExtra(Constants.ORIGIN, Constants.ORIGIN_MANUAL);
         AutoSleepService.enqueue(context, work);
     }
 }
